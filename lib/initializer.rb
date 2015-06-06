092ddbd11f3e2a256d56f18066ce446a7350b77b
Max Kuipers <max.kuipers@gmail.com>
2015-06-06 12:31:15 -0500

    no more env for getting api token

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -3,5 +3,5 @@ require_relative('config_values.rb')
 
 # load config file
 CircleCi.configure do |config|
-  config.token = ENV['CIRCLE_API_TOKEN'] || MyConfig.get('circle_api_token')
+  config.token = MyConfig.get('circle_api_token')
 end
