08234fe1cdbbb9c571174d2d711873f6963bbe0f
Max Kuipers <max.kuipers@gmail.com>
2015-06-06 11:31:50 -0500

    adding configurator thinger

---
+++ lib/config_values.rb
@@ -0,0 +1,46 @@
+require 'pathname'
+require 'yaml'
+
+class Config
+  attr_reader :config
+
+  def self.get(key)
+    #return that value if it exists
+    new.config[key]
+  end
+
+  def self.set(key, value)
+    c = new
+    c.config[key] = value
+    c.write_to_config_file
+  end
+
+  def initialize
+    @config = load_config_file
+  end
+
+  def load_config_file
+    YAML::load_file(File.join(config_dir, "config.yml"))
+  end
+
+  def write_to_config_file
+    c = config_dir
+    if c.nil?
+      #create config dir and file in current directory
+      path = (Pathname.new(".") + ".circle-cli").expand_path
+      path.mkdir
+    end
+    File.open(File.join(c, "config.yml"), 'w') {|f| YAML.dump(@config, f, indentation: 2) }
+  end
+
+  #Recursively look for .circle-cli config directory.
+  def config_dir(dir = Pathname.new("."))
+    app_config_dir = dir + ".circle-cli"
+    if dir.children.include?(app_config_dir)
+      app_config_dir.expand_path
+    else
+      return nil if dir.expand_path.root?
+      config_dir(dir.parent)
+    end
+  end
+end
--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,6 +1,7 @@
 require('circleci')
+require_relative('config_values.rb')
 
 # load config file
 CircleCi.configure do |config|
-  config.token = ENV['API_TOKEN']
+  config.token = ENV['CIRCLE_API_TOKEN'] || Config.get('circle_api_token')
 end
